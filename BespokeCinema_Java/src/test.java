9601d7724922a3073545b68d0644e3852e64834d
smhrd <smhrd@211.223.136.232>
2021-10-21 11:41:27 +0900

    커밋했지롱

--- BespokeCinema_Java/src/test.java
+++ BespokeCinema_Java/src/test.java
@@ -4,6 +4,9 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HI");
+		
+		System.out.println("첫 번째 코드 작성!");
+		System.out.println("두 번째 코드 작성!");
 	}
 
 }
