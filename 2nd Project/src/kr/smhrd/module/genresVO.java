b22edc45477a0530d2bd44ff2aa31c2621ca3469
smhrd <smhrd@221.156.61.73>
2021-10-27 17:15:33 +0900

    moduleVO

---
+++ 2nd Project/src/kr/smhrd/module/genresVO.java
@@ -0,0 +1,20 @@
+package kr.smhrd.module;
+
+public class genresVO {
+	private int genre_seq;
+	private String genre_name;
+	
+	public int getGenre_seq() {
+		return genre_seq;
+	}
+	public void setGenre_seq(int genre_seq) {
+		this.genre_seq = genre_seq;
+	}
+	public String getGenre_name() {
+		return genre_name;
+	}
+	public void setGenre_name(String genre_name) {
+		this.genre_name = genre_name;
+	}
+	
+}
